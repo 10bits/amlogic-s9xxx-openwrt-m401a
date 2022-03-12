1e63695f72e14d308f6e0645ebc047d440ae34e3
zzcand111 <31539914+zzcand111@users.noreply.github.com>
2022-03-12 22:41:12 +0800

    Update diy-part1.sh

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -9,6 +9,7 @@
 # Uncomment a feed source
   sed -i '$a src-git  helloworld https://github.com/fw876/helloworld' feeds.conf.default
   sed -i '$a src-git  OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
+  sed -i '$a src-git uwe5622-aw https://github.com/ChalesYu/uwe5622-aw' feeds.conf.default
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #
